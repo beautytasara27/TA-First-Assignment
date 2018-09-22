748356c9820a3d2e19d097e638da0ede3ff8d203
Beauty Tasara <beautytasara27@gmail.com>
2018-09-22 16:50:49 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TA-First-Assignment
---
+++ TAsession1.py
@@ -0,0 +1,7 @@
+counting = 1
+for i in range(0, 15):
+    for x in range(0, counting):
+        print("* ", end="")
+    counting = counting + 1
+    print()
+
